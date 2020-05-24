fbff23cea5f340dd75048d3411974dcbbcdd912d
wanggang1906 <1294901911@qq.com>
2020-05-24 22:31:30 +0800

    crud详解

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,21 +5,28 @@ import store from '../store'
 import { getToken } from '@/utils/auth'
 import Config from '@/settings'
 
+// 封装axios
+
 // 创建axios实例
+// axios.create返回的是promise
 const service = axios.create({
   baseURL: process.env.NODE_ENV === 'production' ? process.env.VUE_APP_BASE_API : '/', // api 的 base_url
   timeout: Config.timeout // 请求超时时间
 })
 
-// request拦截器 - 接受客户机浏览器提交的数据
+// axios的拦截器interceptors.xx.use(两个参数)
+// request拦截器 - 请求拦截，接受客户机浏览器提交的数据
 service.interceptors.request.use(
+  // 发送成功，验证config中符合服务器的数据，携带特殊信息
   config => {
     if (getToken()) {
       config.headers['Authorization'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
     }
     config.headers['Content-Type'] = 'application/json'
     return config
+    // 返回请求响应，继续请求后台
   },
+  // 发送失败
   error => {
     // Do something with request error
     console.log(error) // for debug
@@ -27,8 +34,10 @@ service.interceptors.request.use(
   }
 )
 
-// response 拦截器 - 将服务器返回的数据发往浏览器
+// response 拦截器 - 响应拦截，将服务器返回的数据发往浏览器
 service.interceptors.response.use(
+
+  // 响应成功
   response => {
     const code = response.status
     if (code < 200 || code > 300) {
@@ -40,6 +49,7 @@ service.interceptors.response.use(
       return response.data
     }
   },
+  // 响应失败
   error => {
     let code = 0
     try {
